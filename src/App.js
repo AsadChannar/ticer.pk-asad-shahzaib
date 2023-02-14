daca3c5a9bcbf8c81b1ede58eda7b880976432b5
AsadChannar <122275667+AsadChannar@users.noreply.github.com>
2023-02-14 05:31:32 -0800

    bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+       <button className='btn btn-success'>success</button>
+
     </div>
   );
 }
