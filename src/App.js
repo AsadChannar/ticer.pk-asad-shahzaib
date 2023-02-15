abc3b7e1b032b810cccbcc1331e35938933b45f0
AsadChannar <122275667+AsadChannar@users.noreply.github.com>
2023-02-15 05:06:50 -0800

    navebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 import './App.css';
 
+import Navebar from './components/Navigation/Navebar'
 function App() {
   return (
-    <div className="App">
-       <button className='btn btn-success'>success</button>
-
+    <div className="App" style={{backgroundImage:"url('./a.jpg')"}}>
+      <Navebar/>
     </div>
   );
 }
